50c69bd853773309ddba75be523f0c14802eb0da
Anonymous969X <ms.anonymaus@gmail.com>
2026-08-12 22:25:37 -0400

    Update rooftop_sale.asm

--- data/items/rooftop_sale.asm
+++ data/items/rooftop_sale.asm
@@ -1,17 +1,25 @@
 RooftopSaleData1:
 	db 5
-	dbw POKE_BALL,     150
-	dbw GREAT_BALL,    500
-	dbw SUPER_POTION,  500
-	dbw FULL_HEAL,     300
-	dbw REVIVE,       1200
+	dbw POKE_BALL,     100
+	dbw GREAT_BALL,    300
+	dbw REPEL,         200
+	dbw ESCAPE_ROPE,   500
+	dbw SUPER_POTION,  350
+	dbw FULL_HEAL,     200
+	dbw REVIVE,       1000
 	db -1
 
 RooftopSaleData2:
 	db 5
-	dbw HYPER_POTION, 1000
-	dbw FULL_RESTORE, 2000
-	dbw FULL_HEAL,     300
 	dbw ULTRA_BALL,    600
-	dbw PROTEIN,      8000
+	dbw HYPER_POTION,  800
+	dbw FULL_RESTORE, 1250
+	dbw FULL_HEAL,     200
+	dbw REVIVE,       1000
+	dbw PROTEIN,      5000
+	dbw IRON,         5000
+	dbw CARBOS,       5000
+	dbw CALCIUM,      5000
+	dbw ZINC,         5000
+	dbw HP_UP,        5000
 	db -1
