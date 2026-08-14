f1a571f076df09c7faf4faf778800026caad137d
Anonymous969X <ms.anonymaus@gmail.com>
2026-08-14 14:05:56 -0400

    Update ralph_callee.asm

--- data/phone/text/ralph_callee.asm
+++ data/phone/text/ralph_callee.asm
@@ -9,51 +9,87 @@ RalphAnswerPhoneText:
 	done
 
 RalphAnswerPhoneDayText:
-	text "Yes? Ah, <PLAYER>."
+	text "Heh."
 
-	para "You're courteous"
-	line "enough to state"
-
-	para "your name when you"
-	line "call on the phone."
+	para "I had a feeling"
+	line "you might call."
 	done
 
 RalphAnswerPhoneNiteText:
-	text "Yes? Ah, <PLAYER>."
+	text "No, I didn't"
+	line "wait long."
 
-	para "You're courteous"
-	line "enough to state"
+	para "Yes, I can see"
+	line "the stars here."
 
-	para "your name when you"
-	line "call on the phone."
+	para "And you?"
+	line "Heh."
+
+	para "How was your day?"
+	line "Sounds tough. Me?"
+	cont "Bored, as always."
 	done
 
 RalphGreetText:
-	text "Hiya, it's Uncle"
+	text "Hey, it's"
 	line ""
 	text_ram wStringBuffer3
 	text "."
 	done
 
 RalphGreetDayText:
-	text "Hiya, it's Uncle"
-	line ""
-	text_ram wStringBuffer3
-	text "."
+	text "What do you"
+	line "want?"
+
+	para "Sh-- sorry."
+	line "Thought you were"
+	cont "someone else."
+
+	para "Yeah, plenty of"
+	line "people call me."
+
+	para "You think you're"
+	line "the only person"
+	cont "I talk to?"
 	done
 
 RalphGreetNiteText:
-	text "Hiya, it's Uncle"
-	line ""
-	text_ram wStringBuffer3
-	text "."
+	text "Evening."
+
+	para "Heh, I won't"
+	line "lecture you."
+
+	para "I called you"
+	line "first, didn't I?"
+
+	para "I'm not that"
+	line "rude. Hey!"
+	cont "Don't laugh!"
+
 	done
 
-RalphGenericText:
+RalphGenericTextMon:
 	text "Are you taking"
 	line "good care of your"
 
 	para "#mon? You have"
 	line "to be good to your"
-	cont "partners!"
+	cont "partner."
+
+	para "…"
+	line "……"
+
+	para "A lot of life"
+	line "is like that."
+	done
+
+RalphGenericTextMom:
+	text "You calling"
+	line "your mom? …"
+
+	para "Don't give me"
+	line "that nonsense."
+
+	para "When I hang up,"
+	line "call her. Got it?"
 	done
